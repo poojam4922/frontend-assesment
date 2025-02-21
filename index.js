f1ff8bba5ce7471a75c136d8bca34099e40e5638
poojam4922 <162030892+poojam4922@users.noreply.github.com>
2025-02-21 10:44:36 +0530

    error resolved

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 document.querySelectorAll(".pricing-container").forEach((container) => {
   const options = container.querySelector(".options-container");
   const input = container.querySelector(".radio input");
-  console.log(input, "input");
 
-  container.addEventListener("mouseenter", () => {
+  container.addEventListener("click", () => {
+    // Hide options for all containers first
+    document
+      .querySelectorAll(".pricing-container")
+      .forEach((otherContainer) => {
+        const otherOptions = otherContainer.querySelector(".options-container");
+        const otherInput = otherContainer.querySelector(".radio input");
+        if (otherOptions !== options) {
+          otherOptions.style.opacity = "0";
+          otherOptions.style.transform = "translateY(-10px)";
+          otherOptions.style.display = "none";
+          otherInput.checked = false;
+        }
+      });
+
+    // Show options for the clicked container
     options.style.display = "flex";
     options.style.opacity = "1";
     options.style.transform = "translateY(0)";
     input.checked = true;
-  });
-
-  container.addEventListener("mouseleave", () => {
-    options.style.opacity = "0";
-    options.style.transform = "translateY(-10px)";
-    options.style.display = "none";
-    input.checked = false;
   });
 });
